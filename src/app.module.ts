e510f30029f7288e87ee23c4e3d57e6203e06c41
owner <owner@MacBook-Pro-owner.local>
2024-02-26 04:20:24 +0600

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ config();
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
+      host: process.env.DB_HOST || '127.0.0.1',
       port: +process.env.DB_PORT || 5432,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
